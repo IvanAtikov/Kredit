675d8a684019884713141cefab932c6fb940b20d
Ivan Atikov <133241007+IvanAtikov@users.noreply.github.com>
2023-06-11 23:28:29 +0700

    variables fixed 2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
 
     public double calculate(int credit, double rate, int term) {
-        double stavkaMonth = rate / (100 * 12);
+        double monthRate = rate / (100 * 12);
         int i = -term;
-        double result = credit * stavkaMonth / (1 - Math.pow((1 + stavkaMonth), i));
+        double result = credit * monthRate / (1 - Math.pow((1 + monthRate), i));
         return result;
     }
 }
